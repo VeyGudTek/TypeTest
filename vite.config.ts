c85f15d666cabf00f81d5351eb4119a70adf15c9
Tim Lin <lintim03@gmail.com>
2026-02-12 19:59:57 -0600

    Add addition path aliasing and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      "@Components": path.resolve(__dirname, './src/Components'),
+      "@CSS": path.resolve(__dirname, './src/CSS'),
+      "@Models": path.resolve(__dirname, './src/Models')
+    }
+  },
   plugins: [react()],
 })
