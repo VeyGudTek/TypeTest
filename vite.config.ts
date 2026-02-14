267dac5ee08c1acb1b8e4bdb3e7ceca3289eabd6
Tim Lin <lintim03@gmail.com>
2026-02-13 23:10:48 -0600

    Update Base with correct path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/TypeTest/",
   resolve:{
     alias:{
       "@Components": path.resolve(__dirname, './src/Components'),
