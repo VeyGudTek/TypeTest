465b284c38758b4401476743a8e6a4d71abb9d20
Tim Lin <lintim03@gmail.com>
2026-02-14 00:13:43 -0600

    Create Addition Tests

--- src/Models/Page.ts
+++ src/Models/Page.ts
@@ -1,4 +1,4 @@
-export type PageOption = "home" | "results" | "0" | "1";
+export type PageOption = "home" | "results" | "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
 export type TestOption = Exclude<PageOption, "home" | "results">;
 
 export interface TestData{
@@ -12,7 +12,39 @@ export const tests:Record<TestOption, TestData> = {
         letterSet: ["a", "s", "d", "f"]
     },
     "1":{
-        display: "jkl;",
-        letterSet: ["j", "k", "l", ";"]
+        display: "qwer",
+        letterSet: ["q", "w", "e", "r"]
+    },
+    "2":{
+        display: "zxcv",
+        letterSet: ["z", "x", "c", "v"]
+    },
+    "3":{
+        display: "asdfqwer",
+        letterSet: ["a", "s", "d", "f", "q", "w", "e", "r"]
+    },
+    "4":{
+        display: "asdfzxcv",
+        letterSet: ["a", "s", "d", "f", "z", "x", "c", "v"]
+    },
+    "5":{
+        display: "asdfg",
+        letterSet: ["a", "s", "d", "f", "g"]
+    },
+    "6":{
+        display: "qwert",
+        letterSet: ["q", "w", "e", "r", "t"]
+    },
+    "7":{
+        display: "zxcvb",
+        letterSet: ["z", "x", "c", "v", "b"]
+    },
+    "8":{
+        display: "asdfgqwertzxcvb",
+        letterSet: ["a", "s", "d", "f", "g", "q", "w", "e", "r", "t", "z", "x", "c", "v", "b"]
+    },
+    "9":{
+        display: "asdf1234",
+        letterSet: ["a", "s", "d", "f", "1", "2", "3", "4"]
     }
 }
